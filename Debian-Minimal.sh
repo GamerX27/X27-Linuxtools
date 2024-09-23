e638b93e38fd19f61916c2e860016dc558dcc903
X27 <87399193+GamerX27@users.noreply.github.com>
2024-09-23 11:28:26 +0200

    Update Debian-Minimal.sh

--- Debian-Minimal.sh
+++ Debian-Minimal.sh
@@ -3,7 +3,7 @@
 
 # Update and install packages
 sudo apt update
-sudo apt install -y curl htop cinnamon-core flatpak papirus-icon-theme synaptic distrobox pysassc build-essential xdg-user-dirs xdg-user-dirs-gtk make
+sudo apt install -y curl htop cinnamon-core flatpak papirus-icon-theme synaptic distrobox pysassc build-essential xdg-user-dirs xdg-user-dirs-gtk make vlc eom
 
 # Add Flathub repository for Flatpak
 echo "Adding Flathub repository for Flatpak..."
