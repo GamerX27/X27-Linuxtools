400c8fb95eeba77350e4fffb4d1902ec8b40483c
X27 <87399193+GamerX27@users.noreply.github.com>
2024-10-13 22:32:01 +0200

    Update Arch-Gnome.sh

--- Arch-Gnome.sh
+++ Arch-Gnome.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Script to install GNOME core on Arch Linux
+# Script to install GNOME core with additional apps on Arch Linux
 
 echo "Updating system..."
 sudo pacman -Syu --noconfirm
 
 echo "Installing GNOME core packages..."
-sudo pacman -S --noconfirm gnome-shell gnome-control-center gnome-session gnome-settings-daemon mutter nautilus gdm
+sudo pacman -S --noconfirm gnome-shell gnome-control-center gnome-session gnome-settings-daemon mutter gdm
+
+echo "Installing additional GNOME applications..."
+sudo pacman -S --noconfirm nautilus gedit totem alacarte evince baobab eog seahorse gnome-translation-editor gnome-terminal
 
 echo "Enabling GDM (GNOME Display Manager)..."
 sudo systemctl enable gdm
@@ -14,5 +17,5 @@ sudo systemctl enable gdm
 echo "Starting GDM..."
 sudo systemctl start gdm
 
-echo "GNOME core installation complete. Rebooting now..."
+echo "GNOME core and additional applications installation complete. Rebooting now..."
 sudo reboot
